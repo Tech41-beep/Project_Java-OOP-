dbf9186ebc1911806ffe855376668c3278b0a187
LovKimtech <Lovkimtech@gmail.com>
2026-01-26 11:21:03 +0700

    this is OOP week2

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args){
         Product latte = new Product("Latte", 3.5);
-        Product americano = new Product("Americano", 2.5);
+        Product americano = new Product("Ameri=cano", 2.5);
 
         latte.printInfo();
         americano.printInfo();
